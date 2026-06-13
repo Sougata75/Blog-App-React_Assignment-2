6333fa0964270d7b9567edc0fc3257bf45a323f2
Sougata75 <bhuniasougata237@gmail.com>
2026-06-13 18:16:05 +0530

    Update 1.1 banner added in home screen and optimised for mobile devices

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,72 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import type { DataType } from "../typescript/interface/blogInterface";
 
 function Home() {
+  const [blogData,setBlogData] = useState<DataType[]>([]);
+  const [error, setError] = useState<string>("");
+  const [loading,setLoading] = useState<boolean>(false);
+  const [slide,setSlide] = useState<number>(0);
+
+  useEffect(() => {
+    const fetchBlogData = async () => {
+      try{
+        const blogs = await axios.get(`https://jsonfakery.com/blogs`);
+        setBlogData(blogs?.data);
+      }catch(error:any){
+        setError(error.message);
+      }finally{
+        setLoading(false);
+      }
+    };
+    fetchBlogData();
+    console.log(error,loading);
+
+    const autoSlider = setInterval(() => {
+      setSlide((item) => {
+        if(item === blogData.length -1){
+          return 0;
+        }
+        return item + 1
+      })
+    },30000);
+
+    return () => clearInterval(autoSlider)
+  },[]);
   return (
-    <div>Home</div>
-  )
+    <>
+      <section
+        className=" mb-[20px] md:mb-[50px] pb-[10px] md:pb-[50px] flex  justify-center bg-cover bg-center"
+        style={{ background: `url(${blogData[slide]?.featured_image})` }}
+      >
+        <div className="md:max-w-[1170px] h-auto md:h-[600px] w-full flex flex-wrap items-center">
+          <div className="flex flex-wrap justify-center md:justify-normal text-center md:text-start gap-6 md:gap-12 md:w-[70%]">
+            <h1 className=" text-[25px] justify-center md:justify-normal md:text-[75px] w-full font-bold text-gray-700 flex items-center gap-4">
+            Welcome To
+            <p className="font-bold text-yellow-500">TechBlog</p>
+          </h1>
+          <div>
+            <p className="text-white text-[8px] md:text-xl px-6 md:px-0">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
+            consectetur maiores unde nostrum dolorum ad optio, autem distinctio
+            natus! Molestias iusto pariatur explicabo qui accusantium aut
+            aliquid, delectus et consectetur. Lorem ipsum dolor sit, amet
+            consectetur adipisicing elit. Voluptas asperiores vel non repellat
+            eius officiis eos voluptates, itaque voluptatum, harum est, delectus
+            sint nobis consequuntur praesentium. Ratione, qui voluptates. Error?
+          </p>
+          </div>
+          <button className='text-yellow-500 border border-white bg-black p-2 md:px-8 md:py-4 text-sm md:text-[18px] rounded-2xl'>Explore More</button>
+          </div>
+
+          <div className="w-[30%] flex flex-col">
+
+          </div>
+        </div>
+      </section>
+      <section className="py-[50px]"></section>
+    </>
+  );
 }
 
-export default Home
+export default Home;
---
+++ src/typescript/interface/blogInterface.ts
@@ -0,0 +1,49 @@
+export interface DataType{
+     id: string,
+     user_id: string,
+     title: string,
+     subtitle: string,
+     summary: string,
+     category: string,
+     featured_image: string,
+     main_content: string,
+     created_at: string,
+     updated_at: string,
+     user: User,
+     comments?: Comments[],
+     tags?: Tags[]
+}
+
+export interface User {
+
+       id: string,
+       first_name: string,
+       middle_name: string,
+       last_name: string,
+      email: string,
+      contact_id: string,
+      address_id: string,
+      username: string,
+      role: string,
+      profile_pic: string,
+      email_verified_at: null,
+      created_at: string,
+      updated_at: string  
+}
+
+export interface Comments {
+        id: string,
+        user_id: string,
+        blog_post_id: string,
+        main_content: string,
+        created_at: string,
+        updated_at: string,
+      }
+
+export interface Tags {
+        id: string,
+        blog_post_id: string,
+        name: string,
+        created_at: string,
+        updated_at: string
+      }
