4ea8d7b0ca2fc2d5cb19ce6d1b77f7ba6e0d179d
Sougata75 <bhuniasougata237@gmail.com>
2026-06-17 17:22:18 +0530

    Update 2.1.4 bug fix testing

--- src/pages/Author.tsx
+++ src/pages/Author.tsx
@@ -49,7 +49,7 @@ function Author() {
       <section className="pb-[15px] md:pb-[50px] flex justify-center">
         <div className="w-full h-[25vh] md:h-[50vh] bg-cover bg-center" style={{backgroundImage: `url(${AuthorBanner})`}}>
           <div className="w-full h-full py-8 md:py-[70px] flex justify-center items-end bg-gradient-to-b from-transparent to-gray-950">
-            <div className="md:max-w-[1170px] h-auto w-full flex flex-wrap justify-center items-center">
+            <div className="w-full md:w-[1170px] h-auto flex flex-wrap justify-center items-center">
                <div className="w-[80%] flex flex-wrap justify-center items-end ">
                  <h1 className=" text-[25px] justify-center md:justify-normal md:text-[75px] w-full font-bold text-gray-500 flex flex-wrap items-center gap-2 md:gap-4">
                   Find Your Favorite 
@@ -61,19 +61,19 @@ function Author() {
         </div>
       </section>
       <section className="py-[15px] md:py-[50px] mb-4 md:mb-0 flex justify-center">
-        <div className="w-full px-2 md:px-0 md:w-[1170px] flex flex-wrap ">
+        <div className="w-full px-1 md:px-0 md:w-[1170px] flex flex-wrap ">
           <div className="w-full border-b border-gray-600 mb-6">
             <h2 className="text-gray-500 font-semibold text-xl mb-2">Authors:</h2>
           </div>
-          <div className="w-full flex flex-wrap gap-4 md:gap-6">
-            {authors?.map((id) => (
-            <div key={id?.id} className="w-full flex flex-wrap justify-between">
+          <div className="w-full flex flex-wrap gap-y-4 md:gap-6">
+            {authors?.slice(0,5).map((id) => (
+            <div key={id?.id} className="w-full flex flex-wrap justify-between bg-gray-900">
               <div className="w-[45%] h-[150px] md:h-[400px] bg-cover bg-center rounded-lg" style={{backgroundImage:`url(${id.profile_pic})`}}></div>
               <div className="w-[52%] md:w-[50%] flex flex-wrap py-1 md:py-5">
                 <div className="w-full flex flex-wrap h-[90px] md:h-[200px]">
                   <h3 className="text-gray-500 text-[16px] md:text-4xl font-black w-full md:mb-3">{id.first_name} {id.middle_name} {id.last_name}</h3>
                   <p className="text-gray-600 font-semibold text-[8px] md:text-xl w-full">User Name: {id.username}</p>
-                  <p className="text-gray-600 font-semibold text-[8px] md:text-xl w-full">Email: {id.email}</p>
+                  <p className="text-gray-600 font-semibold hidden md:block text-[8px] md:text-xl w-full">Email: {id.email}</p>
                   <p className="text-gray-600 font-semibold text-[8px] md:text-lg w-full">Joind From: {id.created_at}</p>
                   <p className="text-gray-600 font-semibold text-[8px] md:text-lg w-full">Role: {id.role}</p>
                 </div>
